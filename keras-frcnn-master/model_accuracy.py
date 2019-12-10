33a962147400899b5741501d30b318efb34fd800
artracy <atracy@colostate.edu>
2019-12-09 23:43:22 -0500

    Hits and Targets combined.
    Woot!

--- keras-frcnn-master/model_accuracy.py
+++ keras-frcnn-master/model_accuracy.py
@@ -368,10 +368,10 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 
 			(real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2) 
 
-			cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(hit_class_to_color[key][0]), int(hit_class_to_color[key][1]), int(hit_class_to_color[key][2])),2)
+			cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(hit_class_to_color[key][0]), int(hit_class_to_color[key][1]), int(hit_class_to_color[key][2])),20)
 
-			textLabel = '{}: {}'.format(key,int(100*new_probs[jk]))
-			all_dets.append((key,100*new_probs[jk]))
+			textLabel = '{}'.format(key)
+			
 
 			(retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
 			textOrg = (real_x1, real_y1-0)
@@ -380,9 +380,6 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 			cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
 			cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
 			
-			print(key)
-			print(x1, y1, x2, y2)
-			print(real_x1, real_y1, real_x2, real_y2)
             
     #############################################
     #  Target boxes
@@ -397,10 +394,9 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 
 			(real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2) 
 
-			cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(target_class_to_color[key][0]), int(target_class_to_color[key][1]), int(target_class_to_color[key][2])),2)
+			cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(target_class_to_color[key][0]), int(target_class_to_color[key][1]), int(target_class_to_color[key][2])),20)
 
-			textLabel = '{}: {}'.format(key,int(100*new_probs[jk]))
-			all_dets.append((key,100*new_probs[jk]))
+			textLabel = '{}'.format(key)			
 
 			(retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
 			textOrg = (real_x1, real_y1-0)
@@ -409,12 +405,9 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 			cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
 			cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
 			
-			print(key)
-			print(x1, y1, x2, y2)
-			print(real_x1, real_y1, real_x2, real_y2)
 
 	print('Elapsed time = {}'.format(time.time() - st))
-	print(all_dets)
+
 	#cv2.imshow('img', img)
 	#cv2.waitKey(0)
 	cv2.imwrite('result\{}_result.png'.format(img_name),img)
